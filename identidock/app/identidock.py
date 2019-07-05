1093734ad6a1d6beac551564117cc6b0310df3d4
mikibouns <mikibouns@gmail.com>
2019-07-05 12:08:35 +0200

    changed file and added comments to file

--- identidock/app/identidock.py
+++ identidock/app/identidock.py
@@ -1,13 +1,20 @@
-from flask import Flask, render_template, Response
+from flask import Flask, Response, request
 import requests
+import hashlib # импортируем библиотеку для хэширования даных
+
 
 app = Flask(__name__)
+salt = "UNIQUE_SALT" # соль для хэш-функции
 default_name = "Igor Matiek"
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST']) # указываем какие медоды будут обрабатываться
 def main_page():
     name = default_name
+    if request.method == 'POST': # если метод POST
+        name = request.form['name'] # получаем значение поля формы
+    salted_name = salt + name # добавляем соль к полученному значению из формы
+    name_hash = hashlib.sha256(salted_name.encode()).hexdigest() # получаем хэшированное значение
     data_html = '''
     <html lang="en">
     <head>
@@ -16,24 +23,24 @@ def main_page():
     </head>
     <body>
         <form method="POST">
-            Hello <input type="text" name="name" value="{}">
+            Hello <input type="text" name="name" value="{0}">
             <input type="submit" value="submit">
         </form>
         <p>
             You look like a:
-            <img src="/monster/monster.png"/>
+            <img src="/monster/{1}"/>
         </p>
     </body>
     </html>
-    '''.format(name)
+    '''.format(name, name_hash) # изменяем значение парамеитра src для тега img
     return data_html
 
 
 @app.route('/monster/<name>')
 def get_identicon(name):
-    r = requests.get('http://dnmonster:8000/monster/{}?size=80'.format(name))
+    r = requests.get('http://dnmonster:8080/monster/{}?size=80'.format(name))
     image = r.content
-    return Response(image, mimetype='img/png')
+    return Response(image, mimetype='image/png')
 
 
 if __name__ == '__main__':

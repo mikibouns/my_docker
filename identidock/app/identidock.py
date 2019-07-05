ba810e8fb345b11b0d282d2bbd09b72c5c849e4d
mikibouns <mikibouns@gmail.com>
2019-07-05 12:51:10 +0200

    delete comments strings

--- identidock/app/identidock.py
+++ identidock/app/identidock.py
@@ -15,24 +15,6 @@ def main_page():
         name = request.form['name'] # получаем значение поля формы
     salted_name = salt + name # добавляем соль к полученному значению из формы
     name_hash = hashlib.sha256(salted_name.encode()).hexdigest() # получаем хэшированное значение
-    # data_html = '''
-    # <html lang="en">
-    # <head>
-    #     <meta charset="UTF-8">
-    #     <title>Identidock</title>
-    # </head>
-    # <body>
-    #     <form method="POST">
-    #         Hello <input type="text" name="name" value="{0}">
-    #         <input type="submit" value="submit">
-    #     </form>
-    #     <p>
-    #         You look like a:
-    #         <img src="/monster/{1}"/>
-    #     </p>
-    # </body>
-    # </html>
-    # '''.format(name, name_hash) # изменяем значение парамеитра src для тега img
     return render_template('index.html', name=name, name_hash=name_hash)
 
 
